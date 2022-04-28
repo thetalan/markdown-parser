e3abb7ae96530b7335f289578f4d11c2ab69dd94
thetalan <103216570+thetalan@users.noreply.github.com>
2022-04-28 12:52:21 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,21 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
+
+import java.util.List;
+
 public class MarkdownParseTest{
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void lab4Testing() {
+        //Path fileName = Path.of(test-file.md);
+        List testList = List.of("test-file.md");
+
     }
 }
